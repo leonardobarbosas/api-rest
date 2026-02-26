31c4289a90a4e4d63bc3a7fe308bcedf6c127d15
leonardobarbosas <leonardobarbosasantos@gmail.com>
2026-02-25 21:55:35 -0300

    Adicionando a controller

---
+++ src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
@@ -0,0 +1,12 @@
+package br.com.fiap.api_rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+
+
+}
--- src/main/java/br/com/fiap/api_rest/repository/ProdutoRepository.java
+++ src/main/java/br/com/fiap/api_rest/repository/ProdutoRepository.java
@@ -7,6 +7,4 @@ import java.util.UUID;
 
 public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
 
-
-
 }
