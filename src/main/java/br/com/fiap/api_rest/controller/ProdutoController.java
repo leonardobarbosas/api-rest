a8cc2cfdfe264d0cfda5927e4a5856a305f85f84
leonardobarbosas <leonardobarbosasantos@gmail.com>
2026-02-25 22:50:02 -0300

    Crud Completo de Produtos

--- src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
@@ -1,12 +1,62 @@
 package br.com.fiap.api_rest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.api_rest.model.Produto;
+import br.com.fiap.api_rest.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoService produtoService;
 
+    @PostMapping
+    public ResponseEntity<Produto> createProduto(@RequestBody Produto produto){
+        Produto produtoSalvo = produtoService.create(produto);
+        return new ResponseEntity<>(produtoSalvo, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> readProduto(@PathVariable UUID id){
+        Produto produto = produtoService.read(id);
+        if (produto == null) {
+            return new ResponseEntity<>(produto, HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity<>(produto, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> readProdutos(){
+        List<Produto> produtos = produtoService.read();
+
+        if (produtos.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(produtos, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto){
+        Produto produtoExistente = produtoService.read(produto.getId());
+        if (produtoExistente == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Produto produtoAtualizado = produtoService.update(produto);
+        return new ResponseEntity<>(produtoAtualizado, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduto(@PathVariable UUID id){
+        produtoService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
