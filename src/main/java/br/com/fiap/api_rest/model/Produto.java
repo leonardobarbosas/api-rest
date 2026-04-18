e5e382c135900d45f5ab9c865c7194cc24f69be6
leonardobarbosas <leonardobarbosantos@gmail.com>
2026-04-17 22:40:26 -0300

    adicionando outras classes

--- src/main/java/br/com/fiap/api_rest/model/Produto.java
+++ src/main/java/br/com/fiap/api_rest/model/Produto.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -19,15 +20,11 @@ public class Produto {
     private BigDecimal preco;
     @Column(name = "data_validade")
     private LocalDate expiracao;
+    private Categoria categoria;
+    private int estoque;
+    @ManyToMany(mappedBy = "produtos")
+    private List<Pedido> pedidos;
 
-    public Produto() {
-    }
-
-    public Produto(String nome, BigDecimal preco, LocalDate expiracao) {
-        this.nome = nome;
-        this.preco = preco;
-        this.expiracao = expiracao;
-    }
 
     public UUID getId() {
         return id;
@@ -59,5 +56,29 @@ public class Produto {
 
     public void setExpiracao(LocalDate expiracao) {
         this.expiracao = expiracao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
     }
 }
