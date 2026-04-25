e1fc453ba86bf557a52546f79f6f39cb61cd8827
leonardobarbosas <leonardobarbosantos@gmail.com>
2026-04-24 22:44:53 -0300

    Criando Usuario

---
+++ src/main/java/br/com/fiap/api_rest/model/UserRole.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api_rest.model;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private final String role;
+
+    UserRole(String role) { this.role = role; }
+
+    public String getRole() { return role; }
+
+}
