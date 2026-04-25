a2a062178392f718c29e3857d16b3a7ec942437e
leonardobarbosas <leonardobarbosantos@gmail.com>
2026-04-24 22:04:08 -0300

    repository

--- src/main/java/br/com/fiap/api_rest/repository/ProdutoRepository.java
+++ src/main/java/br/com/fiap/api_rest/repository/ProdutoRepository.java
@@ -1,8 +1,10 @@
 package br.com.fiap.api_rest.repository;
 
+import br.com.fiap.api_rest.model.Categoria;
 import br.com.fiap.api_rest.model.Produto;
-import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -10,28 +12,36 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
-
-    //SELECT DISTINCT(NOME) FROM PRODUTO;
+    // SELECT DISTINCT(nome) FROM produto;
     Produto findDistinctNomeBy();
 
-    //SELECT * FROM PRODUTO ORDER BY PRECO DESC LIMIT 3;
-    List<Produto> findTop3ByPreco();
+    // SELECT * FROM produto ORDER BY preco DESC LIMIT 3;
+    List<Produto> findTop3ByPrecoAndEstoqueLessThan();
 
-    //SELECT * FROM PRODUTO WHERE NOME = 'NOME'
+    // SELECT * FROM produto WHERE LOWER(nome) = 'nome';
     List<Produto> findByNomeEqualsIgnoreCase(String nome);
     List<Produto> findByNomeIsNot(String nome);
     List<Produto> findByPedidosIsNotNull();
     List<Produto> findByAtivoTrue();
-    //SELECT * FROM PRODUTO WHERE NOME LIKE 'NARG%'
-    List<Produto> findByNomeStartingWith(String prefix); //narg
-    //SELECT * FROM PRODUTO WHERE NOME LIKE '%NARG'
-    List<Produto> findByNomeEndingWith(String suffix); //narg
 
-    //SELECT * FROM PRODUTO WHERE NOME LIKE '%arguil%'
-    List<Produto> findByNomeContaining(String infix); //arguil
-    List<Produto> findByNomeLikeIgnoreCase(String pattern); //a%v%c
+    // SELECT * FROM produto WHERE nome LIKE 'narg%'
+    List<Produto> findByNomeStartingWith(String prefix);
+
+    // SELECT * FROM produto WHERE nome LIKE '%guile'
+    List<Produto> findByNomeEndingWith(String sufix);
+
+    // SELECT * FROM produto WHERE nome LIKE '%guile%'
+    List<Produto> findByNomeIsLike(String infix);
+
+    // SELECT * FROM produto WHERE nome ILIKE 'a%b%c'
+    List<Produto> findByNomeLikeIgnoreCase(String pattern);
     List<Produto> findByPrecoBetween(BigDecimal min, BigDecimal max);
-    List<Produto> findByNomeIn(List<String> nomes); //NOME IN (SELECT...)
+    List<Produto> findByNomeIn(List<String> nome);
     List<Produto> findByExpiracaoBefore(LocalDate dataExpiracao);
-    List<Produto> findByEstoqueExists
+
+    @Query("SELECT p FROM Produto p WHERE p.categoria = :categoria AND p.preco > 1000")
+    List<Produto> encontrarProdutosCaros(@Param("categoria") Categoria categoria);
+
+    @Query(value = "SELECT * FROM TB_PRODUTOS WHERE preco_produto > 100 AND categoria = ?1", nativeQuery = true)
+    List<Produto> encontrarProdutosCaros(String categoria);
 }
